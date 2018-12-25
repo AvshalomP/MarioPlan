1bf055f781ebc01fe7e58fbdb275156b606ff70c
avshalomp <avshi1987@gmail.com>
2018-12-25 23:29:36 +1300

    add signup form

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 
-class SignIn extends Component {
+class SignUp extends Component {
     constructor(props){
         super(props);
 
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            firstName: '',
+            lastName: '',
         }
     }
 
@@ -26,7 +28,7 @@ class SignIn extends Component {
         return (
             <div className="container">
                 <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
+                    <h5 className="grey-text text-darken-3">Sign Up</h5>
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
                         <input type="email" id="email" onChange={this.handleChange}/>
@@ -36,7 +38,15 @@ class SignIn extends Component {
                         <input type="password" id="password" onChange={this.handleChange}/>
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" id="firstName" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="lasttName">Last Name</label>
+                        <input type="text" id="lasttName" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
                     </div>
                 </form>
             </div>
@@ -45,4 +55,4 @@ class SignIn extends Component {
 }
 
 
-export default SignIn;
+export default SignUp;
